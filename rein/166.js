cd5f38e4eb74d9325fe77d26b0d4435db09be939
redeyes2015 <redeyes2015@gmail.com>
2018-09-29 04:39:57 +0000

    166. Fraction to Recurring Decimal
    Special cases, after special cases.

---
+++ rein/166.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number} numerator
+ * @param {number} denominator
+ * @return {string}
+ */
+var fractionToDecimal = function(numerator, denominator) {
+    if (denominator == 0) {
+        return NaN;
+    }
+    if (numerator == 0) {
+        return "0";
+    }
+    const strParts = [];
+    let sign = 1;
+    if (numerator < 0) {
+        numerator = -numerator;
+        sign = -1;
+    }
+    if (denominator < 0) {
+        denominator = -denominator;
+        sign *= -1;
+    }
+    if (sign == -1) {
+        strParts.push('-');
+    }
+    strParts.push(String(Math.floor(numerator / denominator)));
+    let remainder = numerator % denominator;
+    if (remainder == 0) {
+        return strParts.join('');
+    }
+    strParts.push('.');
+    const cycleDetector = new Map();
+    while (remainder > 0) {
+        if (cycleDetector.has(remainder)) {
+            const cycleStart = cycleDetector.get(remainder);
+            strParts[cycleStart] = '(' + strParts[cycleStart];
+            strParts.push(')');
+            break;
+        }
+        cycleDetector.set(remainder, strParts.length);
+        remainder *= 10;
+        const newDigit = Math.floor(remainder / denominator);
+        strParts.push(String(newDigit));
+        remainder = remainder % denominator;
+    }
+    return strParts.join('');
+};
+
+console.log(fractionToDecimal(1, 2), "0.5");
+console.log(fractionToDecimal(2, 1), "2");
+console.log(fractionToDecimal(2, 3), "0.(6)");
+console.log(fractionToDecimal(1, 7), "0.(142857)");
+console.log(fractionToDecimal(-50, 8), "-6.25");
+console.log(fractionToDecimal(-22, -2), "11");
+console.log(fractionToDecimal(22, -2), "-11");
+console.log(fractionToDecimal(1, -7), "-0.(142857)");
+console.log(fractionToDecimal(-1, 7), "-0.(142857)");
+console.log(fractionToDecimal(0, -5), "0");
+console.log(fractionToDecimal(0, 0), "NaN");
